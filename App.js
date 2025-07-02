c6cb59e4ce35e04320c7969510ea607c7ce8660b
Federico <vulcanofederico94@gmail.com>
2025-07-02 17:23:34 +0200

    applicazione style

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.redBordered}>Hello world!!!</Text>
+      <Button title='Tap me!' /> 
     </View>
   );
 }
@@ -17,4 +16,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  redBordered: {
+    borderWidth: 2,
+    borderColor: 'red',
+    margin: 16,
+    padding: 16,
+  }
 });
+  
